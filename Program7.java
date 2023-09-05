ee57195341f985bf429d1b76abea5a76962f3d7c
Jhun Mark <jhunmarkromano2@gmaile.com>
2023-09-05 22:17:24 +0800

    test Run

--- Program7.java
+++ Program7.java
@@ -1,5 +1,8 @@
 public class Program7 {
     public static void main(String[] args) {
-        
+        int a = 5, b = 6;
+
+        System.out.println("A = " + a);
+        System.out.println("B = " + b); 
     }
 }
