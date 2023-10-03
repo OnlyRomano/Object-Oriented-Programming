61a56c1547da882d41972457f2e7e00496564206
OnlyRomano <143976514+OnlyRomano@users.noreply.github.com>
2023-10-03 17:07:49 +0800

    Add files via upload

---
+++ MidtermAct/MidtrermLecture4/shape.java
@@ -0,0 +1,18 @@
+package MidtrermLecture4;
+
+public class shape {
+    private double data1, data2;
+
+    public shape(double Data1, double Data2) {
+        this.data1 = Data1;
+        this.data2 = Data2;
+    }
+
+    public double getData1() {
+        return data1;
+    }
+
+    public double getData2() {
+        return data2;
+    }
+}
